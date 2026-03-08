5f535500afcefd54c4a896ac1eb5d77e891996bd
LaurenceTaylor <morfeus06388casa@gmail.com>
2026-03-07 20:01:33 -0600

    feat: update main.py -problem solved but too much code-

--- main.py
+++ main.py
@@ -16,19 +16,64 @@ def navigate_trail(map):
     matrix_map = []
     for item in map:
         matrix_map.append(list(item))
-    start_row, start_col, rows, cols = find_start_pos(matrix_map)
-    
-    print()
+    pos_row, pos_col, rows, cols = find_start_pos(matrix_map)
+    path = ''
+    prev_col = -1
+    prev_row = -1
+    is_the_end = False
+    while not is_the_end:
+        if pos_row - 1 >= 0 and pos_row-1 != prev_row: 
+            if matrix_map[pos_row - 1][pos_col] == 'T': 
+                path += 'U'
+                prev_row = pos_row
+                prev_col = -1
+                pos_row -= 1
+            elif matrix_map[pos_row - 1][pos_col] == 'G': 
+                path += 'U'
+                is_the_end = True
+                pos_row -= 1
+        if pos_row + 1 < rows and pos_row+1 != prev_row: 
+            if matrix_map[pos_row + 1][pos_col] == 'T': 
+                path += 'D'
+                prev_row = pos_row
+                prev_col = -1
+                pos_row += 1
+            elif matrix_map[pos_row + 1][pos_col] == 'G': 
+                path += 'D'
+                is_the_end = True
+                pos_row += 1
+        if pos_col - 1 >= 0 and prev_col != pos_col-1: 
+            if matrix_map[pos_row][pos_col - 1] == 'T': 
+                path += 'L'
+                prev_col = pos_col
+                prev_row = -1
+                pos_col -= 1
+            elif matrix_map[pos_row][pos_col - 1] == 'G': 
+                path += 'L'
+                is_the_end = True
+                pos_col -= 1
+        if pos_col + 1 < cols and prev_col != pos_col+1: 
+            if matrix_map[pos_row][pos_col + 1] == 'T': 
+                path += 'R'
+                prev_col = pos_col
+                prev_row = -1
+                pos_col += 1
+            elif matrix_map[pos_row][pos_col + 1] == 'G': 
+                path += 'R'
+                is_the_end = True
+                pos_col += 1
+
+    print(path)
 
     return map
 
 if __name__ == '__main__':
-    print(navigate_trail(["-CT--", "--T--", "--TT-", "---T-", "---G-"]))
+    #print(navigate_trail(["-CT--", "--T--", "--TT-", "---T-", "---G-"]))
     print('-----')
-    print(navigate_trail(["-----", "--TTG", "--T--", "--T--", "CTT--"]))
+    #print(navigate_trail(["-----", "--TTG", "--T--", "--T--", "CTT--"]))
     print('-----')
-    print(navigate_trail(["-C----", "TT----", "T-----", "TTTTT-", "----G-"]))
+    #print(navigate_trail(["-C----", "TT----", "T-----", "TTTTT-", "----G-"]))
     print('-----')
     print(navigate_trail(["--------", "-CTTT---", "----T---", "---GT---", "--------"]))
     print('-----')
-    print(navigate_trail(["TTTTTTT-", "T-----T-", "T-----T-", "TTTT--TG", "---C----"]))
+    #print(navigate_trail(["TTTTTTT-", "T-----T-", "T-----T-", "TTTT--TG", "---C----"]))
